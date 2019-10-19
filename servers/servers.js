faa1d8bd0600cdd5c497bab43b292be5c1b8d43c
iPro <38023207+iPro36@users.noreply.github.com>
2019-10-18 22:29:10 -0400

    Update servers.js

--- servers/servers.js
+++ servers/servers.js
@@ -1,16 +1,21 @@
 exports.run = (bot, msg, args, command, Discord, ms, moment, prefix) =>
 {
-    if (msg.guild.id == '274678517730508802')
-    {
-        //iPro
-        let ipro = require(`./ipro/ipro.js`);
-        ipro.run(bot, msg, args, command, Discord, ms, moment, prefix);
-    }
+    if (msg.author.bot || !msg.content.startsWith(prefix)) return;
 
-    if (msg.guild.id == '272954876021964800')
+    else
     {
-        //Rn
-        let rn = require(`./rn/rn.js`);
-        rn.run(bot, msg, args, command, Discord, ms, moment, prefix);
+        if (msg.guild.id == '274678517730508802')
+        {
+            //iPro
+            let ipro = require(`./ipro/ipro.js`);
+            ipro.run(bot, msg, args, command, Discord, ms, moment, prefix);
+        }
+    
+        if (msg.guild.id == '272954876021964800')
+        {
+            //Rn
+            let rn = require(`./rn/rn.js`);
+            rn.run(bot, msg, args, command, Discord, ms, moment, prefix);
+        }
     }
 }
