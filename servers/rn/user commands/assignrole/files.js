93ddd8844040242daeb08958a1d53e8409f9972d
iPro <38023207+iPro36@users.noreply.github.com>
2019-11-16 17:03:16 -0500

    Update files.js

--- servers/rn/user commands/assignrole/files.js
+++ servers/rn/user commands/assignrole/files.js
@@ -1,7 +1,4 @@
 exports.run = (bot, msg, args, command, Discord, ms, moment, prefix) =>
 {
-    //Call of Duty
-    let cod = require(`./cod.js`);
-    cod.run(bot, msg, args, command, Discord, ms, moment, prefix);
-
+    
 }
